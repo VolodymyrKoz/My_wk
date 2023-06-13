4c714380758bf62eb7d4723e2ba126dec4034bc6
VolodymyK <114539981+VolodymyK@users.noreply.github.com>
2023-06-13 11:53:31 -0500

    update

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const menuButton = document.querySelector(".menu-button");
-const mobileMenu = document.querySelector(".mobile-menu");
-const closeButton = document.querySelector("#close-menu");
 
-menuButton.addEventListener("click", function () {
-  mobileMenu.style.display = "block";
-});
-
-closeButton.addEventListener("click", function () {
-  mobileMenu.style.display = "none";
-});
